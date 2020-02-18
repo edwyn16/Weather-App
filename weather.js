e63aa16807a481b2bd1bcae486cf911e0b7f7048
edwyn16 <37575092+edwyn16@users.noreply.github.com>
2020-02-18 11:37:33 +0100

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,37 +1,20 @@
+// gets my current location
 function getLocation() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showLocation);
     } else {
         console.log("Failed");
     }
+    loadStoreCity();
 }
 
-function fetchData(url, current = false) {
-    fetch(url)
+// hier return ik mijn fetched data
+async function fetchData(url, current = false) {
+    return fetch(url)
         .then(function(response) {
             if(response.ok) {
-                response.json().then(function(data) {
-
-                    var div = document.createElement("div");
-                    div.classList.add("cards");
-
-                    var tempp = "<p> Temperture: " + data.main.temp + "°C" + "</p>";
-                    var coords = "<p> Naam van de stad: " + data.name + "</p>";
-                    var loc = "<p> Coordinaten: " + data.coord.lat + ", " + data.coord.lon + "</p>";
-
-                    div.innerHTML += tempp;
-                    div.innerHTML += coords;
-                    div.innerHTML += loc;
-
-                    if(current) {
-
-                        weather[0].innerHTML += tempp;
-                        coord[0].innerHTML += coords;
-                        location[0].innerHTML += loc;
-
-                        return;
-                    }
-                    return render(div);
+                return response.json().then(function(data) {
+                    return data;
                 });
             } else {
                 console.log("response failed");
@@ -39,32 +22,75 @@ function fetchData(url, current = false) {
         });
 }
 
+// hier heb ik een function die mijn nieuwe divs aanmaakt & mijn gefetched data in die div zet.
+function newDiv(data) {
+    const div = document.createElement("div");
+    div.classList.add("cards");
+
+    const tempp = "<p> Temperture: " + data.main.temp + "°C" + "</p>";
+    const coords = "<p> Naam van de stad: " + data.name + "</p>";
+    const loc = "<p> Coordinaten: " + data.coord.lat + ", " + data.coord.lon + "</p>";
+
+    div.innerHTML += tempp;
+    div.innerHTML += coords;
+    div.innerHTML += loc;
+    return render(div);
+}
+
+// hier is
 function render(element) {
     return document.body.appendChild(element);
 }
 
-function showLocation(position) {
+async function showLocation(position) {
     console.log(position);
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
     const url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=e10a28dc8f6f5e57908fa1073297b8ec&units=metric";
-    fetchData(url);
+    let data = await fetchData(url, true);
+    console.log(data);
+    newDiv(data);
 }
 
 function getInputValue() {
-    document.querySelector(".button").addEventListener("click", function () {
-        var value = document.querySelector(".addCity").value;
+    document.querySelector(".button").addEventListener("click", async function () {
+        const value = document.querySelector(".addCity").value;
         document.querySelector(".weather").append(value);
         const url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=e10a28dc8f6f5e57908fa1073297b8ec&units=metric";
         console.log(url);
-
-        // let myObject_Serialized = JSON.stringify(url);
-        // console.log(myObject_Serialized);
-
-        fetchData(url);
+        let data = await fetchData(url);
+        newDiv(data);
+        storeCity(value);
     });
 }
+
+function storeCity(value) {
+    // Get the current items
+    let city = JSON.parse(localStorage.getItem("cityName"));
+    // If no items, create Array
+    if (!city) {
+      city = [];
+    }
+    city.push(value);
+    localStorage.setItem("cityName", JSON.stringify(city));
+}
+
+function loadStoreCity() {
+    // Get the items
+    const city = JSON.parse(localStorage.getItem("cityName"));
+    // If there are no items, do nothing
+    if (!city) {
+      return false;
+    }
+        city.forEach(async function(value){
+            console.log(value);
+            const url =  "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + value + "&appid=e10a28dc8f6f5e57908fa1073297b8ec&units=metric";
+
+            let data = await fetchData(url);
+            newDiv(data);
+        });
+    }
 
 getLocation();
 getInputValue();
